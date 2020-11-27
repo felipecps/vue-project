e9b93e29691af492fe590a03096cca087719871f
Felipe Xavier <felipexavier@gmail.com>
2020-11-27 20:27:11 -0300

    Frontend atualizado com vue chamadas sendo feitas para o pythonanywhere

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import 'bootstrap/dist/css/bootstrap.css';
+import BootstrapVue from 'bootstrap-vue';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+new Vue({
+    router,
+    render: h => h(App),
+  }).$mount('#app');
--- src/views/PaginaDoFelipe.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="fefe">
-    <h1>Teste Felipe e Fernanda</h1>
-  </div>
-</template>
