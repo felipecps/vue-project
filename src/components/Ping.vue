cb21ff1f5561208f6f7d08858cedbc870957e20b
Felipe Xavier <felipexavier@gmail.com>
2020-11-28 11:10:52 -0300

    Revert "Tentativa de entender o _redirects"
    This reverts commit 62a61cf29e25db7153c9b79c5ce6422e8e8de2cf.

---
+++ src/components/Ping.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'https://felipecps.pythonanywhere.com/hora_atual';
+      axios.get(path)
+        .then((res) => {
+          console.log(res.data)
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,33 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Ping from '../components/Ping.vue';
+import Books from '../components/Books.vue';
+import Test from '../components/Test.vue';
+
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Books',
+    component: Books,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/ping',
+    name: 'Ping',
+    component: Ping,
   },
   {
-    path: "/pagfelipe",
-    name: "Pagina Do Felipe",
-    component: () =>
-      import("../views/PaginaDoFelipe.vue")
-  }
+    path: '/test',
+    name: 'Test',
+    component: Test,
+  },
 ];
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
