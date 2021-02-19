6547affb71417e3e5dbf17135283c991e8481465
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-02-19 20:01:14 -0300

    Mudando alguns valores para dentro do form

--- src/components/CalculaCorretagem.vue
+++ src/components/CalculaCorretagem.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="container">
         <b-form>
-            <b-form-group id="input-group-1" label="Digite os dados da operação:">
+            <b-form-group id="input-group-1" label="Digite os dados darrr operação:" label-for="input-quantidade">
                 <div>
                     <b-form inline>
                         <b-form-input class="mb-2 mr-sm-2 mb-sm-0"
                                       id="input-codigo-da-acao"
-                                      v-model="acao"
+                                      v-model="form.acao"
                                       placeholder="Código da Ação"
                                       required></b-form-input>
                         <b-form-input class="mb-2 mr-sm-2 mb-sm-0"
                                       id="input-quantidade"
-                                      v-model="quantidade"
+                                      v-model="form.quantidade"
                                       placeholder="Quantidade"
                                       required></b-form-input>
                         <b-form-input class="mb-2 mr-sm-2 mb-sm-0"
                                       id="input-preco"
-                                      v-model="preco"
+                                      v-model="form.preco"
                                       placeholder="Preço unitário"
                                       required></b-form-input>
                         <label class="mr-sm-2" for="inline-form-custom-select-corretora">Compra/Venda</label>
                         <b-form-select id="inline-form-custom-select-tipo"
                                        class="mb-2 mr-sm-2 mb-sm-0"
-                                       v-model="selected"
+                                       v-model="form.selected_compra_venda"
                                        :options="options"></b-form-select>
                         <b-button class="mr-1" v-on:click="addToTable" variant="primary">Adicionar</b-button>
                         <b-button v-on:click="resetAllFields" variant="danger">Limpar</b-button>
@@ -54,16 +54,18 @@
         name: 'CalculaCorretagem',
         data() {
             return {
+                form: {
+                    acao: '',
+                    quantidade: '',
+                    tipo_de_operacao: '',
+                    selected_compra_venda: null,
+                    preco: '',
+                },
                 dismissSecs: 5,
                 dismissCountDown: 0,
                 showDismissibleAlert: false,
-                acao: '',
                 valor_reais: '',
-                quantidade: '',
-                preco: '',
-                tipo_de_operacao: '',
-                selected: null,
-                fields: ['Acao', 'Quantidade', 'Preco', 'Tipo_da_Operacao', 'Valor'],
+                fields: ['Ação', 'Quantidade', 'Preco', 'Tipo_da_Operacao', 'Valor'],
                 options: [
                     { value: null, text: 'Escolha...' },
                     { value: 'Compra', text: 'Compra' },
@@ -84,20 +86,20 @@
                 this.dismissCountDown = dismissCountDown
             },
             addToTable() {
-                if (this.acao != '' && this.quantidade != '' && this.preco != '' && this.selected != null) {
-                    this.preco_editado = this.preco.replace(/,/g, '.')
-                    let temp = this.quantidade * this.preco_editado
+                if (this.form.acao != '' && this.form.quantidade != '' && this.form.preco != '' && this.form.selected_compra_venda != null) {
+                    this.preco_editado = this.form.preco.replace(/,/g, '.')
+                    let temp = this.form.quantidade * this.preco_editado
                     this.valor_reais = this.converter(temp)
-                    this.items.push({ Acao: this.acao, Quantidade: this.quantidade, Preco: this.preco, Tipo_da_Operacao: this.selected, Valor:this.valor_reais })
+                    this.items.push({ Ação: this.form.acao, Quantidade: this.form.quantidade, Preco: this.form.preco, Tipo_da_Operacao: this.form.selected_compra_venda, Valor:this.valor_reais })
                 }
                 else {
                     this.showAlert()
                 }
             },
             resetAllFields() {
-                this.acao = ''
-                this.quantidade = ''
-                this.preco = ''
+                this.form.acao = ''
+                this.form.quantidade = ''
+                this.form.preco = ''
             }
         },
         created() {
