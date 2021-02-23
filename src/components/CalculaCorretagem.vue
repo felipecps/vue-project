06113a19c4da69fb8d1a27885c40dddaee33706c
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-02-23 14:46:55 -0300

    adicionando cards

--- src/components/CalculaCorretagem.vue
+++ src/components/CalculaCorretagem.vue
@@ -1,49 +1,66 @@
 <template>
     <div class="container">
-        <b-form>
-            <b-form-group id="input-group-1" label="Digite os dados darrr operação:" label-for="input-quantidade">
-                <div>
-                    <b-form inline>
-                        <b-form-input class="mb-2 mr-sm-2 mb-sm-0"
-                                      id="input-codigo-da-acao"
-                                      v-model="form.acao"
-                                      placeholder="Código da Ação"
-                                      required></b-form-input>
-                        <b-form-input class="mb-2 mr-sm-2 mb-sm-0"
-                                      id="input-quantidade"
-                                      v-model="form.quantidade"
-                                      placeholder="Quantidade"
-                                      required></b-form-input>
-                        <b-form-input class="mb-2 mr-sm-2 mb-sm-0"
-                                      id="input-preco"
-                                      v-model="form.preco"
-                                      placeholder="Preço unitário"
-                                      required></b-form-input>
-                        <label class="mr-sm-2" for="inline-form-custom-select-corretora">Compra/Venda</label>
-                        <b-form-select id="inline-form-custom-select-tipo"
-                                       class="mb-2 mr-sm-2 mb-sm-0"
-                                       v-model="form.selected_compra_venda"
-                                       :options="options"></b-form-select>
-                        <b-button class="mr-1" v-on:click="addToTable" variant="primary">Adicionar</b-button>
-                        <b-button v-on:click="resetAllFields" variant="danger">Limpar</b-button>
-                    </b-form>
-                </div>
-            </b-form-group>
-            <br>
+        <div>
+            <b-card bg-variant="light" text-variant="black">
+                <label for="input-valor">Valor:</label>
+                <b-form-input id="input-valor"
+                              v-model="valor_nota"
+                              placeholder="Qual é o valor da nota?"
+                              required></b-form-input>
+
+                <b-form-group class="mt-3" label="Corretora:" v-slot="{ ariaDescribedby }">
+                    <b-form-radio-group id="radio-group-1"
+                                        v-model="corretora"
+                                        :options="options_corretora"
+                                        :aria-describedby="ariaDescribedby"
+                                        name="radio-options"></b-form-radio-group>
+                </b-form-group>
+            </b-card>
+        </div>
 
 
-            <b-alert :show="dismissCountDown"
-                     dismissible
-                     fade
-                     variant="warning"
-                     @dismiss-count-down="countDownChanged">
-                Você precisa preencher todos os campos antes de adicionar!!
-            </b-alert>
 
-            <div>
-                <b-table striped hover :items="items" :fields="fields"></b-table>
-            </div>
-        </b-form>
+
+
+        <div>
+            <b-card class="mt-3" bg-variant="light" text-variant="black">
+                <b-form>
+                    <label for="input-codigo-da-acao">Ação</label>
+                    <b-form-input id="input-codigo-da-acao"
+                                  v-model="form.acao"
+                                  placeholder="Qual é o código da ação?"
+                                  required></b-form-input>
+                </b-form>
+                <label class="mt-3" for="input-quantidade">Quantidade</label>
+                <b-form-input id="input-quantidade"
+                              v-model="form.quantidade"
+                              placeholder="Quantas ações você comprou ou vendeu?"
+                              required></b-form-input>
+                <label class="mt-3" for="input-preco">Preço unitário</label>
+                <b-form-input id="input-preco"
+                              v-model="form.preco"
+                              placeholder="Qual foi o preço unitário?"
+                              required></b-form-input>
+                <label class="mt-3" for="inline-form-custom-select-tipo">Compra/Venda</label>
+                <b-form-select id="inline-form-custom-select-tipo"
+                               v-model="form.selected_compra_venda"
+                               :options="options"></b-form-select>
+                <b-button class="mt-3" v-on:click="addToTable" variant="primary">Adicionar</b-button>
+                <b-button class="mt-3" v-on:click="resetAllFields" variant="danger">Limpar</b-button>
+            </b-card>
+        </div>
+
+        <b-alert :show="dismissCountDown"
+                 dismissible
+                 fade
+                 variant="warning"
+                 @dismiss-count-down="countDownChanged">
+            Você precisa preencher todos os campos antes de adicionar!!
+        </b-alert>
+
+        <div>
+            <b-table striped hover :items="items" :fields="fields"></b-table>
+        </div>
     </div>
 </template>
 
@@ -61,11 +78,16 @@
                     selected_compra_venda: null,
                     preco: '',
                 },
+                corretora: '',
+                options_corretora: [
+                    { value: 'xp', text: 'XP' },
+                    { value: 'easynvest', text: 'Easynvest' }
+                ],
                 dismissSecs: 5,
                 dismissCountDown: 0,
                 showDismissibleAlert: false,
                 valor_reais: '',
-                fields: ['Ação', 'Quantidade', 'Preco', 'Tipo_da_Operacao', 'Valor'],
+                fields: ['Ação', 'Quantidade', 'Preco', 'Tipo_da_Operacao', 'Taxa de Liquidação', 'Valor'],
                 options: [
                     { value: null, text: 'Escolha...' },
                     { value: 'Compra', text: 'Compra' },
@@ -100,6 +122,9 @@
                 this.form.acao = ''
                 this.form.quantidade = ''
                 this.form.preco = ''
+                this.form.selected_compra_venda = null
+                this.corretora = ''
+                this.option_corretora = null
             }
         },
         created() {
